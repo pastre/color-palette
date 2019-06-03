d491722e209ed59738084e213dd5a9dafcb132c9
pastre <pastre68@gmail.com>
2019-06-03 10:43:51 -0300

    Initial Commit

---
+++ Color Palette/Color_Palette-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Color_Palette-Bridging-Header.h
+//  Color Palette
+//
+//  Created by Bruno Pastre on 03/06/19.
+//  Copyright © 2019 Bruno Pastre. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
